3619df4e2103a3a4d61edd1c4044ce86f652977b
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-07-10 17:05:26 +0200

    finish camera rotation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -12,13 +12,19 @@ public class CameraControl : MonoBehaviour
     private float rotation;
     private float verticalRotation;
 
-
+    private void Start()
+    {
+        verticalRotation = verticalPoint.localRotation.eulerAngles.x;
+    }
 
     void Update()
     {
         transform.position = target.position;
+        
         rotation += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         verticalRotation += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, 0f, 75f);
+        
         transform.rotation = Quaternion.Euler(0f, rotation, 0f);
         verticalPoint.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
     }
