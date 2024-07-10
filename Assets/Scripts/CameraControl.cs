6b785b2049322ff615ad72c4507494004e4482ca
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-07-10 16:57:32 +0200

    camera rotation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,23 +7,19 @@ public class CameraControl : MonoBehaviour
     public Transform target;
     public Transform verticalPoint;
 
-    public float moveSpeed = 5f;
+    public float moveSpeed = 180f;
  
     private float rotation;
     private float verticalRotation;
 
 
 
-    void Start()
-    {
-     
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = target.position;
-        //rotation += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        //transform.rotation = Quaternion.Euler(18f, rotation, 0);
+        rotation += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        verticalRotation += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0f, rotation, 0f);
+        verticalPoint.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
     }
 }
