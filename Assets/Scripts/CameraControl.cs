35d9dd20748d69341b3b17718a044bb97f066955
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-07-10 17:19:35 +0200

    camera mouse rotation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,6 +7,7 @@ public class CameraControl : MonoBehaviour
     public Transform target;
     public Transform verticalPoint;
 
+    public bool useMouseRotation = false;
     public float moveSpeed = 180f;
  
     private float rotation;
@@ -15,14 +16,23 @@ public class CameraControl : MonoBehaviour
     private void Start()
     {
         verticalRotation = verticalPoint.localRotation.eulerAngles.x;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     void Update()
     {
         transform.position = target.position;
-        
-        rotation += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        verticalRotation += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        if(useMouseRotation)
+        {
+            rotation += Input.GetAxis("Mouse X") * moveSpeed * Time.deltaTime;
+            verticalRotation += Input.GetAxis("Mouse Y") * moveSpeed * Time.deltaTime;
+        } else
+        {
+            rotation += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+            verticalRotation += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        }
+       
         verticalRotation = Mathf.Clamp(verticalRotation, 0f, 75f);
         
         transform.rotation = Quaternion.Euler(0f, rotation, 0f);
